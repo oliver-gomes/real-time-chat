8ac3d887f4d5f8a74540ddc4ae305a1470f8f258
Oliver Gomes <ogomesphoto@gmail.com>
2019-10-06 17:33:21 -0400

    router dom add

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Join from "./components/Join";
+import Chat from "./components/Chat";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route path="/" exact component={Join}></Route>
+      <Route path="/chat" exact component={Chat}></Route>
+    </Router>
   );
 }
 
