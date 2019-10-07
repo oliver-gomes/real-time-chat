cdd941a1854e9f3ec49e91ce5df4e627636f7e37
Oliver Gomes <ogomesphoto@gmail.com>
2019-10-07 19:54:54 -0400

    code cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import Join from "./components/Join/Join";
 import Chat from "./components/Chat/Chat";
 
