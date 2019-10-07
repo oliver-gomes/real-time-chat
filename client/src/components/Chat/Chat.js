b7326f0a18a49893a32ee39d1a1ea9f9a60f3ee1
Oliver Gomes <ogomesphoto@gmail.com>
2019-10-07 19:54:39 -0400

    set room and name

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
 
-export default function Chat() {
-    return (
-        <div>
-            <h1>Chat</h1>
-        </div>
-    )
+let socket;
+
+export default function Chat({ location }) {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const ENDPOINT = "localhost:5000";
+
+  socket = io(ENDPOINT);
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+
+    setName(name);
+    setRoom(room);
+
+    console.log(socket);
+  });
+  return (
+    <div>
+      <h1>Chat</h1>
+    </div>
+  );
 }
